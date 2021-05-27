6f7d78f240cbb11c057bf152390cb1aeb5c5b9ca
Marina Lifanova <deliorno@gmail.com>
2021-05-27 20:31:16 +0300

    for server

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/mesto-react"> 
+    <BrowserRouter> 
     <App />
     </BrowserRouter> 
   </React.StrictMode>,
