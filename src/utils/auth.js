a0333d502054d9dcb6ff74bc7d2fd2ea54beff98
Marina Lifanova <deliorno@gmail.com>
2021-05-28 15:11:41 +0300

    request on my back

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://api.deliorno.mesto-react.nomoredomains.icu';
 
 const handleOriginalResponse = (response) => {
   if (response.ok){
