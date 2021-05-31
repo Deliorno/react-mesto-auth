caecb48b74617ba992f9141eab740ca4514b0b35
Marina Lifanova <deliorno@gmail.com>
2021-05-31 11:02:10 +0300

    for back

--- src/utils/api.js
+++ src/utils/api.js
@@ -21,8 +21,8 @@ const handleOriginalResponse = (res) => {
 class Api {
     constructor() {
       //super(props);
-        this._urlCards = "https://api.deliorno.mesto-react.nomoredomains.icu/cards";
-        this._urlUserInfo = "https://api.deliorno.mesto-react.nomoredomains.icu/users/me";
+        this._urlCards = "https://deliorno.mesto-react.nomoredomains.icu";
+        this._urlUserInfo = "https://deliorno.mesto-react.nomoredomains.icu";
         // this._headers = {
         //     'content-type': 'application/json',
         //     'Authorization': `Bearer ${localStorage.getItem('jwt')}`
